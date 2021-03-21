58f18025602eb05d86e4f734afa95cc11e31fadd
goncy <gonzalo.pozzo4@gmail.com>
2021-03-21 10:51:57 -0300

    Fix width

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -31,65 +31,66 @@ const Window: React.FC<Props> = ({onClose, dragConstraints, title, children}) =>
   }
 
   return (
-    <motion.div
-      drag={canDrag}
-      dragConstraints={dragConstraints}
-      dragElastic={false}
-      dragMomentum={false}
-      style={{width: "fit-content"}}
-    >
-      <Stack
-        backgroundColor="gray.700"
-        borderColor="gray.600"
-        borderRadius="md"
-        borderWidth={1}
-        boxShadow="xl"
-        maxWidth={{base: "90vw", md: 640}}
+    <Box maxWidth={{base: "90vw", md: 640}}>
+      <motion.div
+        drag={canDrag}
+        dragConstraints={dragConstraints}
+        dragElastic={false}
+        dragMomentum={false}
+        style={{width: "100%"}}
       >
-        <Stack alignItems="center" direction="row" padding={2} position="relative">
-          <Stack alignItems="center" direction="row" position="absolute">
-            <Box
-              backgroundColor="red.500"
-              borderRadius={9999}
-              cursor="pointer"
-              height={3}
-              tabIndex={0}
-              width={3}
-              onClick={onClose}
-              onKeyPress={(e) => ["Enter", "Space"].includes(e.code) && onClose()}
-            />
-            <Box
-              backgroundColor="yellow.500"
-              borderRadius={9999}
-              height={3}
-              opacity={0.25}
-              width={3}
-            />
-            <Box
-              backgroundColor="green.500"
-              borderRadius={9999}
-              height={3}
-              opacity={0.25}
-              width={3}
-            />
+        <Stack
+          backgroundColor="gray.700"
+          borderColor="gray.600"
+          borderRadius="md"
+          borderWidth={1}
+          boxShadow="xl"
+        >
+          <Stack alignItems="center" direction="row" padding={2} position="relative">
+            <Stack alignItems="center" direction="row" position="absolute">
+              <Box
+                backgroundColor="red.500"
+                borderRadius={9999}
+                cursor="pointer"
+                height={3}
+                tabIndex={0}
+                width={3}
+                onClick={onClose}
+                onKeyPress={(e) => ["Enter", "Space"].includes(e.code) && onClose()}
+              />
+              <Box
+                backgroundColor="yellow.500"
+                borderRadius={9999}
+                height={3}
+                opacity={0.25}
+                width={3}
+              />
+              <Box
+                backgroundColor="green.500"
+                borderRadius={9999}
+                height={3}
+                opacity={0.25}
+                width={3}
+              />
+            </Stack>
+            <Text
+              color="gray.400"
+              flex={1}
+              fontWeight="bold"
+              textAlign="center"
+              userSelect="none"
+              onPointerDown={handleDragHandleDown}
+              onPointerEnter={handleDragHandleEnter}
+              onPointerLeave={handleDragHandleLeave}
+              onPointerUp={handleDragHandleUp}
+            >
+              {title}
+            </Text>
           </Stack>
-          <Text
-            color="gray.400"
-            flex={1}
-            fontWeight="bold"
-            textAlign="center"
-            userSelect="none"
-            onPointerDown={handleDragHandleDown}
-            onPointerEnter={handleDragHandleEnter}
-            onPointerLeave={handleDragHandleLeave}
-            onPointerUp={handleDragHandleUp}
-          >
-            {title}
-          </Text>
+          {children}
         </Stack>
-        {children}
-      </Stack>
-    </motion.div>
+      </motion.div>
+    </Box>
   );
 };
 
